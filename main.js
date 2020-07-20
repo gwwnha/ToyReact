598bd581c846554a361246349f2919e680d31ad0
gww <274189715@qq.com>
2020-07-20 22:00:21 +0800

    vdom

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-import { ToyReact } from './ToyReact.js';
+import { ToyReact, Component } from './ToyReact.js';
 
-let a = <div>
-    <span>1</span>
-    <span>2</span>
-    <span>3</span>
-</div>
-console.log('a', a);
-document.body.appendChild(a);
+// let a = <div>
+//     <span>1</span>
+//     <span>2</span>
+//     <span>3</span>
+// </div>
+// console.log('a', a);
+// document.body.appendChild(a);
+
+class MyComponent extends Component {
+    render() {
+        return <div>cool<span> god!!!</span>
+            {this.children} / {true}
+            </div>
+    }
+}
+let a = <MyComponent name="a" id="id"><div>i am children</div></MyComponent>
+
+
+ToyReact.render(a, document.body);
